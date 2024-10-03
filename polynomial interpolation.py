b27ad485b0185a02a6ebe36b67e7e2d6b6e43c35
HP <afonso.canibulas@gmail.com>
2024-10-03 02:12:54 +0100

    aula de IPCE

--- polynomial interpolation.py
+++ polynomial interpolation.py
@@ -1,29 +1,38 @@
 import matplotlib
 from sympy import symbols
 
-def polynomial_interpolation(degree):
-    data_set = []
-    L = [1] * (degree + 1)
-    x = symbols("x")
-    p = 0 # Lagrange Polynomial of n degree
-    for z in range(degree + 1):
-        xi = int(input(f"Insert x value: "))
-        yi = int(input(f"Insert y value: "))
-        data_set.append((xi, yi))
-    x_values = [x for x, y in data_set] # List of x values
-    y_values = [y for x, y in data_set] # List of y values
-    for i in range(0, degree + 1):
-        for k in range(0, degree + 1):
-            if i != k:
-                L[i] = L[i] * (x - x_values[k]) / (x_values[i] - x_values[k]) # Calculation of L parameters
+def polynomial_interpolation(x_values, y_values, degree):
+    p = 0
     for x in range(degree + 1):
-        p = p + (y_values[x] * L[x]) # Polynomial Calculation
+        p += y_values[x] * l_func(x_values, x, degree) # Polynomial Calculation
     print(p)
+
+
+def l_func(x_values, x, degree):
+    l = 1
+    x = symbols("x")
+    for i in range(degree + 1):
+        for k in range(degree + 1):
+            if i != k:
+                l *= (x - x_values[k]) / (x_values[i] - x_values[k])  # Calculation of L parameters
+    return l
+
+
+def plot_graph(f):
+    print("ola")
 
 
 def main():
     n = int(input("> "))
-    polynomial_interpolation(n)
+    # get x y from user input
+    data_set = []
+    for z in range(n + 1):
+        xi = int(input(f"Insert x value: "))
+        yi = int(input(f"Insert y value: "))
+        data_set.append((xi, yi))
+    x_values = [x for x, y in data_set]  # List of x values
+    y_values = [y for x, y in data_set]  # List of y values
+    polynomial_interpolation(x_values, y_values, n)
 
 main()
 
