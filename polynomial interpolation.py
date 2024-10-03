67911c366467471ee71912224422d42ca6c5d265
HP <afonso.canibulas@gmail.com>
2024-10-03 03:42:25 +0100

    aula de IPCE

--- polynomial interpolation.py
+++ polynomial interpolation.py
@@ -1,11 +1,5 @@
-import matplotlib
+import matplotlib as plt
 from sympy import symbols
-
-def polynomial_interpolation(x_values, y_values, degree):
-    p = 0
-    for x in range(degree + 1):
-        p += y_values[x] * l_func(x_values, x, degree) # Polynomial Calculation
-    print(p)
 
 
 def l_func(x_values, x, degree):
@@ -14,25 +8,37 @@ def l_func(x_values, x, degree):
     for i in range(degree + 1):
         for k in range(degree + 1):
             if i != k:
-                l *= (x - x_values[k]) / (x_values[i] - x_values[k])  # Calculation of L parameters
+                l *= (x - x[i]) / (x[k] - x[i])  # Calculation of L parameters
     return l
 
 
-def plot_graph(f):
-    print("ola")
+def polynomial_interpolation(x, y, degree):
+    p = 0
+    for x in range(degree + 1):
+        p += y[x] * l_func(x, x, degree) # Polynomial Calculation
+    return p
 
+
+def graph_plot(x, y, p, degree):
+    plt.plot(polynomial_interpolation(x, y, degree))
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("Polynomial Interpolation")
+    plt.show()
 
 def main():
+    x = []
+    y = []
     n = int(input("> "))
-    # get x y from user input
-    data_set = []
+    # get x y data from user
     for z in range(n + 1):
         xi = int(input(f"Insert x value: "))
         yi = int(input(f"Insert y value: "))
-        data_set.append((xi, yi))
-    x_values = [x for x, y in data_set]  # List of x values
-    y_values = [y for x, y in data_set]  # List of y values
-    polynomial_interpolation(x_values, y_values, n)
+        x.append(xi)
+        y.append(yi)
+    polynomial_interpolation(x, y, n)
+
 
 main()
+
 
