290ce90c57a36819a496085bcac1faea1812376f
Afonso <afonso.canibulas@gmail.com>
2024-10-04 17:35:30 +0100

    done :)

--- polynomial interpolation.py
+++ polynomial interpolation.py
@@ -1,43 +1,50 @@
-import matplotlib as plt
-from sympy import symbols
+import matplotlib.pyplot as plt
+from sympy import symbols, simplify
+from numpy import linspace
 
-print("ola")
-def l_func(x_values, x, degree):
+
+def l_func(x_values, k, n):
     l = 1
     x = symbols("x")
-    for i in range(degree + 1):
-        for k in range(degree + 1):
-            if i != k:
-                l *= (x - x[i]) / (x[k] - x[i])  # Calculation of L parameters
+    for i in range(n + 1):
+        if i != k:
+            # Only one loop because the second parameter of the products comes from the sum
+            l *= (x - x_values[i]) / (x_values[k] - x_values[i])  # Calculation of L parameters
     return l
 
 
-def polynomial_interpolation(x, y, degree):
+def polynomial_interpolation(x_var, y, z):
     p = 0
-    for x in range(degree + 1):
-        p += y[x] * l_func(x, x, degree) # Polynomial Calculation
+    for k in range(z + 1):
+        p += y[k] * l_func(x_var, k, z) # Polynomial Calculation
     return p
 
 
-def graph_plot(x, y, p, degree):
-    plt.plot(polynomial_interpolation(x, y, degree))
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.title("Polynomial Interpolation")
+def graph_plot(x, y, p, f):
+    plt.figure(figsize=(11, 8))
+    manager = plt.get_current_fig_manager()
+    manager.window.title("Lagrange Polynomial Interpolation")
+    plt.xlabel("x", fontsize=1)
+    plt.ylabel("y",  fontsize=1)
+    plt.title("Polynomial Interpolation", fontsize=16, fontweight="bold")
+    x_values = linspace(min(x), max(x), 10)
+    y_values = [polynomial_interpolation(x, y, f).subs('x', val) for val in x_values]
+    y_values = [float(val) for val in y_values]
+    plt.plot(x_values, y_values)
     plt.show()
 
+
 def main():
-    x = []
-    y = []
+    x_data_set = []
+    y_data_set = []
     n = int(input("> "))
-    # get x y data from user
+    # get x, y data from user
     for z in range(n + 1):
         xi = int(input(f"Insert x value: "))
         yi = int(input(f"Insert y value: "))
-        x.append(xi)
-        y.append(yi)
-    polynomial_interpolation(x, y, n)
-
+        x_data_set.append(xi)
+        y_data_set.append(yi)
+    graph_plot(x_data_set, y_data_set, simplify(polynomial_interpolation(x_data_set, y_data_set ,n)), n)
 
 main()
 
